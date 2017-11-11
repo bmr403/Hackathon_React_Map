680674585951e39f1aa69bebbe0c60afb1bad06a
Manjunatha Reddy <bmr403@gmail.com>
2017-11-11 23:15:02 +0530

    Intial commit of Hackathon React

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import API from '../../api/API'
+import MapData from '../../../data.json';
+import invariant from 'invariant';
+
+ const style = {
+  width: '80%',
+  height: '80%',
+  position: 'relative'
+}
+
+export class MapContainer extends Component {
+	constructor(props){
+    super(props);
+    this.state = {
+		 markers: MapData.markers
+    }
+  }
+	renderMarkers() {
+    return MapData.markers.map(marker =>{
+      return <Marker key={marker.name}
+                name={marker.name}
+                marker={marker}
+                position={{lat: marker.lat, lng: marker.lng}}
+              />
+    })
+  }
+	
+	
+render() {
+    return (
+	<Map google={this.props.google} 
+	  style={style}  
+	initialCenter={{
+            lat: 12.8929388,
+            lng: 79.7470399
+          }}  
+		className={'main'}
+	  zoom={10}>
+			
+	{this.renderMarkers()}
+      </Map>
+	  
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyA4SqALhZOzZV8-yijgOGEO6mbBM1xvaT0'
+})(MapContainer) 
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+  entry: './src/App.js',
+  output: {
+    path: './public',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './public'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+  { test: /\.json$/, loader: 'json' },
+    ]
+  }
+};
+
+
